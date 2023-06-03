44174b7e6b0baad9c7a74adc97c563d52aa1d63d
DAPotapov <103317598+DAPotapov@users.noreply.github.com>
2023-06-03 12:25:37 +0300

    Convertion from predecessors to successors made. Inclusion of tasks made.

--- connectors.py
+++ connectors.py
@@ -1,8 +1,9 @@
 import pprint
+import re
 import untangle
 import json
 
-from numpy import busday_offset, datetime64
+from numpy import busday_offset, busday_count, datetime64
 # For testing purposes
 from pprint import pprint
 
@@ -177,6 +178,8 @@ def compose_tasks_list(list, task, allocations):
     
     # TODO in v.2: project apps supports alternative calendars, bot should support them as well.
     # Construct end date from start date, duration and numpy function:
+    # TODO it calculates in between: on a begining of end date task already must be done, end date not exactly the day of deadline
+    # Should be reconsidered: GanttProject itself calculates right
     enddate = str(busday_offset(datetime64(task['start']), int(task['duration']), roll='forward'))
 
     list.append({
@@ -228,6 +231,8 @@ def load_xml(fp):
 
     obj = untangle.parse(str(fp))
 
+    #TODO consider adding version check if difficulties ocurred 
+
     # Check for telegram username field in XML provided
     if 'ExtendedAttribute' in obj.Project.ExtendedAttributes:
         for attr in obj.Project.ExtendedAttributes.ExtendedAttribute:
@@ -264,32 +269,67 @@ def load_xml(fp):
     # Add collected actioners to project dictionary
     project['actioners'] = actioners
 
+    # Gathering tasks from XML
     if 'Task' in obj.Project.Tasks:
-
-
-
-        # Add tasks from XML to list of tasks
         # Because 1st element in MS Project XML format represents project itself so skip it
         for i, task in enumerate(obj.Project.Tasks.Task):
             if i == 0:
                 continue            
-            # print(task.Name.cdata)
+            
+            # Calculate duration from start and end dates using only business days 
+            # and considering that end date (aka deadline) should be included
+            # Calculating duration from presented duration in XML (in hours) may provide not right results: 
+            # actually its effort - assume work only need 1 hour but sometime during the week
+            # And milestones have zero duration
+            if task.Milestone.cdata == '1':
+                duration = 0
+            else:
+                duration = busday_count(xml_date_conversion(task.Start.cdata), xml_date_conversion(task.Finish.cdata)) + 1
+            # print(f"Start: {task.Start.cdata}, End: {task.Finish.cdata}, duration: {duration}")
+
+            # Offset should be recalculated from LinkLag, which expressed in tenth of minutes (sic!), so 4800 is 8 hours
+            # Type of link: Values are 0=FF, 1=FS, 2=SF and 3=SS (docs at https://learn.microsoft.com/en-us/office-project/xml-data-interchange/xml-schema-for-the-tasks-element?view=project-client-2016)
+            # Consider using this one instead of Gantt (and change in that function)
+            include = []
+
             tasks.append({
-                'id': int(task.ID.cdata),
+                'id': int(task.UID.cdata),
+                'WBS': task.WBS.cdata, # this field is useful for determining inclusion of tasks
                 'name': task.Name.cdata,
-                'startdate': task.Start.cdata, # date need to be converted
-                'enddate': task.Finish.cdata, # date need to be converted
-                # 'duration': int(task['duration']), # need to be calculated
-                # 'successors': successors, # # need to be calculated from PredecessorLink
-                'milestone': task.Milestone.cdata, # need to be converted to boolean
+                'startdate': xml_date_conversion(task.Start.cdata), 
+                'enddate': xml_date_conversion(task.Finish.cdata),
+                'duration': duration,
+                'successors': [], # # need to be calculated from PredecessorLink
+                'milestone': True if task.Milestone.cdata == '1' else False, 
                 'complete': int(task.PercentComplete.cdata),
                 'curator': '', # not using for now, but it may become usefull later
-                'basicplan_startdate': task.Start.cdata, # equal to start date on a start of the project
-                'basicplan_enddate': task.Finish.cdata, # date need to be converted # equal to end date on a start of the project
-                # 'include': include,  # need to be calculated from WBS and OutlineLevel
-                # 'actioners': actioners # will be calculated separately
+                'basicplan_startdate': xml_date_conversion(task.Start.cdata), # equal to start date on a start of the project
+                'basicplan_enddate': xml_date_conversion(task.Finish.cdata), # date need to be converted # equal to end date on a start of the project
+                'include': [],  # need to be calculated from WBS and OutlineLevel
+                'actioners': [] # will be calculated separately
             })
             # pprint(tasks[i-1])
+
+        # Start loop again to 
+        for task in obj.Project.Tasks.Task:
+            # find predecessors and fill them
+            if 'PredecessorLink' in task:
+                for record in tasks:
+                    for predecessor in task.PredecessorLink:
+                        # pprint(predecessor)
+                        if record['id'] == int(predecessor.PredecessorUID.cdata):
+                            record['successors'].append(int(task.UID.cdata))
+            
+            # And to find included tasks
+            if task.Type.cdata == '0':
+                # Look at outline level to comprehend level of WBS to look for - more than one
+                if int(task.OutlineLevel.cdata) > 1:
+                # Take part of WBS of task before last dot - this is WBS of Parent task
+                    parentWBS = task.WBS.cdata[:task.WBS.cdata.rindex('.')]
+                    # Find in gathered tasks list task with such WBS and add UID of task to 'include' sublist
+                    for record in tasks:
+                        if record['WBS'] == parentWBS:
+                            record['include'].append(int(task.UID.cdata))
 
     else:        
         raise ValueError('There are no tasks in provided file. Nothing to do.')
@@ -299,6 +339,19 @@ def load_xml(fp):
 
     return project
 
+def xml_date_conversion(datestring):
+    """
+    Convert Project dateTime format (2023-05-15T08:00:00) to inner date format (2023-05-15)
+    """
+
+    # Given date from XML, expect dateTime Project format
+    datestring = re.search('^\d{4}-\d{2}-\d{2}', datestring)
+    if not datestring:
+        raise ValueError (f"Error in start date found ({datestring})")
+    else:
+        datestring = datestring.group()
+
+    return datestring
 
 
 if __name__ == '__main__':
